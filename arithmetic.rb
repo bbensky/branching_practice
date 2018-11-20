b7213630c1d7f628d1c83df5f3baf781875eff80
Brian <bbensky@users.noreply.github.com>
2018-11-19 20:19:17 -0700

    Create arithmetic.rb

---
+++ arithmetic.rb
@@ -0,0 +1,20 @@
+def add(n1, n2)
+  return
+end
+
+def subtract(n1, n2)
+  return
+end
+
+def multiply(n1, n2)
+  return
+end
+
+def divide(n1, n2)
+  return
+end
+
+puts plus(1, 1)
+puts subtract(1, 1)
+puts multiply(1, 1)
+puts divide(1, 1)
